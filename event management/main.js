bfacef78f095ba1a8553182e6dce73c9b1eb48ad
akashkrp20 <35109888+akashkrp20@users.noreply.github.com>
2018-01-04 22:54:03 +0530

    Add files via upload

---
+++ event management/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	
+  $("#loading").delay(2000).fadeOut(500);
+  /*$("#loading-center").click(function() {
+  $("#loading").fadeOut(500);
+  })*/
+
+  $('#myCarousel').carousel({
+interval: 4000
+});
+
+});
+
+
+
