c2a718a458d16ace4803003a82fc369522fbf3e8
akashkrp20 <35109888+akashkrp20@users.noreply.github.com>
2018-01-09 12:06:13 +0530

    Add files via upload

---
+++ editor/js/scrolling-nav.js
@@ -0,0 +1,47 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($("#lstemp0006-navbar .navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("#lstemp0006-navbar top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("#lstemp0006-navbar top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
